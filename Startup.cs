3fe4f6bf38d65bd54c780ece9f8c2258b06db311
DN-98 <deahnisadna@gmail.com>
2022-03-30 22:05:06 +0700

    landing page

--- Startup.cs
+++ Startup.cs
@@ -109,6 +109,9 @@ namespace PaymentAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentAPI v1"));
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseAuthentication();
 
             app.UseHttpsRedirection();
